989b4e63bb5dcbb7f2f6f495c08c02e8d72d2aac
unknown <viniciusdeab@outlook.com>
2022-02-22 19:51:40 -0300

    Arquivo script.js para realização do movimento da página

---
+++ script.js
@@ -0,0 +1,7 @@
+let switchCtn = document.querySelector('#switch-cnt');
+let switchC1 = document.querySelector('#switch-c1');
+let switchC2 = document.querySelector('#switch-c2');
+let switchCircle = document.querySelectorAll('.switch__circle');
+let switchBtn = document.querySelectorAll('.switch-btn');
+let aContainer = document.querySelectorAll('.switch__circle');
+
